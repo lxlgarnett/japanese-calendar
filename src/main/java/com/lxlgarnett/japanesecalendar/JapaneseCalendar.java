a347e2dae2de487123812726640ba899430aa95c
lxlgarnett <lxlgarnett@gmail.com>
2024-02-07 20:46:07 +0800

    Add a method to check the given date is Sunday or not.

--- src/main/java/com/lxlgarnett/japanesecalendar/JapaneseCalendar.java
+++ src/main/java/com/lxlgarnett/japanesecalendar/JapaneseCalendar.java
@@ -22,4 +22,20 @@ public class JapaneseCalendar {
     // Print the days of the week header
     System.out.println("Su Mo Tu We Th Fr Sa");
   }
+
+  /**
+   * Is the given date is Sunday or not.
+   * @param year Year of the given date
+   * @param month Month of the given date
+   * @param day Day of the given date
+   * @return true: Sunday false: Others
+   */
+  public boolean isSunday(int year, int month, int day) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(Calendar.YEAR, year);
+    calendar.set(Calendar.MONTH, month - 1);
+    calendar.set(Calendar.DATE, day);
+
+    return calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY;
+  }
 }
